76f0531dd90118ddb87be80337f025759944a0ae
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 17:22:57 +0100

    defined, applied styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,35 @@
 import React from "react";
 
 const Navigation = (props) => {
+	const ulStyling = {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-around",
+
+		margin: 0,
+		padding: 0,
+		borderBottom: "#775577 solid 1px",
+		listStyleType: "none",
+		backgroundColor: "#DDAADD",
+		textAlign: "center",
+	};
+
+	const liStyling = {
+		flex: 1,
+
+		borderRight: "#775577 solid 1px",
+		padding: "10px 0",
+		textAlign: "center"
+	};
+
 	const navigationItems = props.tabs.map((tab, index) => {
 		return (
-			<li key={index} className={tab.toLowerCase()}>{tab}</li>
+			<li key={index} className={tab.toLowerCase()} style={liStyling}>{tab}</li>
 		);
 	});
 
 	return (
-		<ul>
+		<ul style={ulStyling}>
 			{ navigationItems }
 		</ul>
 	);
