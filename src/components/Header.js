5d726c7093ce300a100a087c9ff004a9b3bf2527
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 17:12:20 +0100

    defined, applied styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,20 @@ import Navigation from "./Navigation";
 const Header = (props) => {
 	const tabs = ["Signup", "Login", "Database"];
 
+	const headerStyling = {
+		backgroundColor: "#AADDAA"
+	};
+
+	const headingStyling = {
+		margin: 0,
+		borderBottom: "#22AA22 solid 1px",
+		padding: "8px 0px",
+		textAlign: "center"
+	};
+
 	return (
-		<header>
-			<h1>{props.name}</h1>
+		<header style={headerStyling}>
+			<h1 style={headingStyling}>{props.name}</h1>
 			<Navigation tabs={tabs}/>
 		</header>
 	);
