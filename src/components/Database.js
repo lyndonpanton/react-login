ed295c0dd6b9e6d5304ae1429b81bf55a6eaad76
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 19:01:40 +0100

    changed tabl styling

--- src/components/Database.js
+++ src/components/Database.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Database = (props) => {
 	const tableStyling = {
-		margin: "10px auto",
+		margin: "30px auto",
 		border: "#000000 solid 1px",
 		borderCollapse: "collapse",
+		borderRadius: "4px",
 		width: "95%",
 		textAlign: "center"
 	};
