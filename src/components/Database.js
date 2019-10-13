ccf92e5eff76b1f8052d9a2678b61d2745bc0877
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 16:52:11 +0100

    created component template

---
+++ src/components/Database.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Database = (props) => {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<td>Username</td>
+					<td>Password</td>
+					<td>Delete</td>
+				</tr>
+			</thead>
+			<tbody>
+
+			</tbody>
+			<tfoot>
+				<tr>
+					<td colSpan="3">This table is rendered using ReactJS</td>
+				</tr>
+			</tfoot>
+		</table>
+	);
+};
+
+export default Database;
