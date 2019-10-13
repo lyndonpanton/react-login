9fb18cb3f11af2d167d1197a7ac395378ef82c21
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 16:16:01 +0100

    changed elements return by render depending on logged in state of app component

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,20 +37,33 @@ class Login extends React.Component {
 
 	render() {
 		const { username, password } = this.state;
+		const loggedIn = (this.props.loggedIn);
 
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<label htmlFor="l-username">
-					<input type="text" name="username" value={username} onChange={this.handleChange} />
-				</label>
+		if (!loggedIn) {
+			return (
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="l-username">
+						<input type="text" name="username" value={username} onChange={this.handleChange} />
+					</label>
 
-				<label htmlFor="l-password">
-					<input type="password" name="password" value={password} onChange={this.handleChange} />
-				</label>
+					<label htmlFor="l-password">
+						<input type="password" name="password" value={password} onChange={this.handleChange} />
+					</label>
 
-				<input type="submit" value="Login" />
-			</form>
-		);
+					<input type="submit" value="Login" />
+				</form>
+			);
+		} else {
+			return (
+				<div>
+					<h2>Welcome Back</h2>
+					<div>
+						<p>Your username is: </p>
+						<p>Your password is: </p>
+					</div>
+				</div>
+			);
+		}
 	};
 };
 
