e4ddbd672effaf5fc689d32bba2db2b81f014feb
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 18:18:01 +0100

    defined, applied footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Footer = (props) => {
 	const { author, rights, year } = props.creation;
-	
+
+	const footerStyling = {
+		marginTop: "10px",
+		padding: "80px 10px",
+		backgroundColor: "#FAFAFA",
+		textAlign: "center",
+		color: "#767676"
+	};
+
 	return (
-		<footer>
+		<footer style={footerStyling}>
 			<p>&copy; {year} {author} | {rights}</p>
 		</footer>
 	);
