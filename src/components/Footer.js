9a8a74ed84566371b4e14ed68f5a5554227c00fa
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-13 18:06:48 +0100

    created, exported component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => {
+	return (
+		<footer>
+			<p>&copy; 2019 Lyndon Panton | All Rights Reserved</p>
+		</footer>
+	);
+};
+
+export default Footer;
